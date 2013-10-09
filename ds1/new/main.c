fb8ae968965ed6fde96f51263b7a2c107140d2bf
anhong <anhong@deb7>
2013-10-09 02:05:20 -0400

    configure the csupport.vim and timestamp.vim

---
+++ ds1/new/main.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *  Last Modified: 10/09/2013 02:05:10 AM
+ *
+ *    Description:  entry of the program
+ *
+ *        Version:  1.0
+ *        Created:  10/09/2013 02:04:55 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ANHONG
+ *          Email:  anhonghe@gmail.com
+ *   Organization:  USTC
+ *
+ * =====================================================================================
+ */
+
+
---
+++ ds1/new/main.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *  Last Modified: 10/09/2013 02:04:50 AM
+ *
+ *    Description:  as wanted
+ *
+ *        Version:  1.0
+ *        Created:  10/09/2013 02:03:38 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ANHONG
+ *          Email:  anhonghe@gmail.com
+ *   Organization:  USTC
+ *
+ * =====================================================================================
+ */
+
+
+#ifndef  __main_inc__
+#define  __main_inc__
+#include "game.h"
+#include <stdio.h>
+#endif   /* ----- #ifndef __main_inc__  ----- */
