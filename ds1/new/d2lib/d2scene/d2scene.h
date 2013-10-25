7e021ad5fd7a7d25d26fa894733670a29ab00d66
anhong <anhong@anhong-deb7.syr.edu>
2013-10-24 20:52:31 -0400

    ok

---
+++ ds1/new/d2lib/d2scene/d2scene.h
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename: d2scene.h
+ *        Created: 10/24/2013 08:46:56 PM
+ *  Last Modified: 10/24/2013 08:52:18 PM
+ *
+ *    Description: define d2scene
+ *
+ *        Version: 1.0
+ *       Revision: none
+ *       Compiler: gcc
+ *
+ *         Author: ANHONG
+ *          Email: anhonghe@gmail.com
+ *   Organization: USTC
+ *
+ * =====================================================================================
+ */
+
+
+#ifndef  _included_d2scene_inc__
+#define  _included_d2scene_inc__
+
+
+
+
+struct D2SCENE {
+    uint32_t        act;
+    uint32_t        width;
+    uint32_t        height;
+};
+typedef struct D2SCENE D2SCENE;
+
+
+
+
+#endif
