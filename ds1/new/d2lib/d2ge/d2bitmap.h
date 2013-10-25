4b816df4a11be9c697a562532db670a563ddc347
anhong <anhonghe@gmail.com>
2013-10-25 15:21:21 -0400

    ok

---
+++ ds1/new/d2lib/d2ge/d2bitmap.h
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename: d2bitmap.h
+ *        Created: 10/25/2013 03:08:14 PM
+ *  Last Modified: 10/25/2013 03:11:01 PM
+ *
+ *    Description: definition of d2bitmap, and all operation based on it
+ *
+ *        Version: 1.0
+ *       Revision: none
+ *       Compiler: gcc
+ *
+ *         Author: ANHONG
+ *          Email: anhonghe@gmail.com
+ *   Organization: USTC
+ *
+ * =====================================================================================
+ */
+
+
+
+#ifndef  _included_d2bitmap_inc__
+#define  _included_d2bitmap_inc__
+
+
+
+
+
+
+
+
+#endif
