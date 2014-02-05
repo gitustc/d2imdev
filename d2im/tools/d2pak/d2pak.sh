ecf0faaf0169e0a01ced7d2ec6f790c6ff0eb497
anhong <anhong@anhong-deb7.syr.edu>
2014-02-05 12:34:31 -0500

    ok

--- d2im/tools/d2pak/d2pak.sh
+++ d2im/tools/d2pak/d2pak.sh
@@ -16,9 +16,45 @@
 # global const
 
 TMP_FOLDER_PATH="./tmp"
-
 INPUT_FOLDER_PATH=$1
+FILE_COUNT_IN_FOLDER=0
 
+
+
+#define functions here
+
+
+########################################################################
+# input: seed
+#      : tmp/list
+# outpt: tmp/hashv_seed
+
+calc_hashv(){
+    cp $TMP_FOLDER_PATH/list $TMP_FOLDER_PATH/calc_hash
+    sed -i "s@.*@./mpqhash $1 &@" $TMP_FOLDER_PATH/calc_hash
+    chmod +x $TMP_FOLDER_PATH/calc_hash
+    $TMP_FOLDER_PATH/calc_hash > $TMP_FOLDER_PATH/hashv_$1
+    rm $TMP_FOLDER_PATH/calc_hash -f
+}
+########################################################################
+
+
+
+
+########################################################################
+# test capacity
+test_capacity(){
+    
+}
+########################################################################
+
+
+
+
+
+
+########################################################################
+# test parameters
 if [[ $# < 1 ]]
 then
     echo ./d2pak folder_name
@@ -30,21 +66,27 @@ then
     echo  $1 does not exist ...
     exit 1
 fi
+########################################################################
 
+
+
+########################################################################
 # step one
 # generate the file name list in tmp
-
 rm -rf $TMP_FOLDER_PATH
 mkdir $TMP_FOLDER_PATH
 find $1 -type f > $TMP_FOLDER_PATH/list
+FILE_COUNT_IN_FOLDER=`awk '{print NR}' $TMP_FOLDER_PATH/list | tail -1`
+########################################################################
 
 
 
+########################################################################
 # setp two
 # calculate the hash value of it
-
-sed -i 's@.*@./mpqhash 1 &@' $TMP_FOLDER_PATH/list
-chmod +x $TMP_FOLDER_PATH/list
-$TMP_FOLDER_PATH/list > $TMP_FOLDER_PATH/hashv
-
-
+for seed in 1
+do
+    calc_hashv $seed
+    awk '{printf("%u\n", $0 % 10)}' $TMP_FOLDER_PATH/hashv_$seed
+done
+########################################################################
